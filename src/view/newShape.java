696a8f808af9a6b00612417fd3fa41cc05da8291
U-DS\wyb15135 <wyb15135@CIS-MUIR-26.cis.strath.ac.uk>
2018-01-24 12:16:52 +0000

    better dialogue layout

--- src/view/newShape.java
+++ src/view/newShape.java
@@ -4,29 +4,49 @@ import javax.swing.*;
 import java.awt.*;
 
 public class newShape {
-    JPanel panShape;
+    JPanel panDI;
 
     public newShape(JFrame fr){
-        String[] actions = {"change colour", "rotate", "move flipper", "idk"};
-        String[] triggers = {"key press", "other shape collision", "self collision", "idk"};
-        JLabel lb = new JLabel("shape icon");
+        String[] actions = {"change colour", "rotate", "connect to other gizmo", "idk"};
+        String[] triggers = {"key press", "connect to other gizmo", "connect to self", "idk"};
+        JLabel label = new JLabel("shape icon");
         JComboBox actionList = new JComboBox(actions);
         JComboBox triggerList = new JComboBox(triggers);
         JTextField position = new JTextField();
-        JColorChooser shapeColour = new JColorChooser();
 
-        panShape = new JPanel();
+        JColorChooser shapeColour = new JColorChooser();
+        shapeColour.setPreviewPanel(new JPanel()); // removes preview pane;
+
+        JPanel panShape = new JPanel();
         panShape.setLayout(new GridLayout(0,2));
 
-        panShape.add(lb);
-        panShape.add(actionList);
-        panShape.add(position);
-        panShape.add(triggerList);
+        JPanel panForm = new JPanel();
+        panForm.setLayout(new BoxLayout(panForm, BoxLayout.Y_AXIS));
+        panForm.setOpaque(false);
+
+        panForm.add(label);
+        panForm.add(actionList);
+        panForm.add(position);
+        panForm.add(triggerList);
+
+        panShape.add(panForm);
         panShape.add(shapeColour);
-        panShape.setBackground(Color.ORANGE);
+        panShape.setOpaque(false);
+
+        JPanel panControls = new JPanel();
+        JButton ok = new JButton("Ok");
+        panControls.add(ok);
+        panControls.setOpaque(false);
+
+        panDI = new JPanel();
+        panDI.add(panShape);
+        panDI.add(panControls);
+        panDI.setBackground(Color.ORANGE);
+        panDI.setLayout(new BoxLayout(panDI, BoxLayout.Y_AXIS));
 
         JDialog shape =  new JDialog(fr, "Add a Gizmo", true);
-        shape.setContentPane(panShape);
+        shape.setContentPane(panDI);
+        shape.setMinimumSize(new Dimension(400,200));
         shape.setVisible(true);
     }
 }
