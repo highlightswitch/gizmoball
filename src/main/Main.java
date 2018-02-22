e47d05fd56fa8e7b2a5d10c89590c81e157ae37f
Chadha <shatha.degachi@gmail.com>
2018-02-22 15:56:19 +0000

    fixes

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,20 +20,16 @@ public class Main {
 		}
 
 		Model model = new Model();
-
 		model.placeGizmo(GizmoType.BALL, "ball", model.getTileAt(5,5));
 		Flipper leftFlipper =  (Flipper) model.placeGizmo(GizmoType.LEFT_FLIPPER, "leftFlipper", model.getTileAt(9,10));
 		Flipper rightFlipper =  (Flipper) model.placeGizmo(GizmoType.RIGHT_FLIPPER, "rightFlipper", model.getTileAt(14,10));
-
 		model.placeGizmo(GizmoType.CIRCLE_BUMPER, "circle",model.getTileAt(5, 18));
 		model.placeGizmo(GizmoType.SQUARE_BUMPER, "square", model.getTileAt(5, 2));
 		model.placeGizmo(GizmoType.TRIANGLE_BUMPER,"triangle", model.getTileAt(8, 12));
 		model.placeGizmo(GizmoType.TRIANGLE_BUMPER, "triangle", model.getTileAt(12, 18));
 
 		model.setUpActionMap(leftFlipper, rightFlipper);
-
 		Absorber absorber = (Absorber) model.placeGizmo(GizmoType.ABSORBER, "absorber", model.getTileAt(0, 19));
-
 		model.setUpActionMap(absorber);
 		GameFrame fr = new GameFrame(model);
 	}
