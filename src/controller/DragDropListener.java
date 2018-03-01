31c16357e5f3710c4ebc884f2bb55c35f4859978
Luca <luca.demelas.2015@uni.strath.ac.uk>
2018-03-01 15:39:36 +0000

    added drag and drop listener and transfer handler for detecting the mouse dragging an image

---
+++ src/controller/DragDropListener.java
@@ -0,0 +1,20 @@
+package controller;
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+    public class DragDropListener extends MouseAdapter {
+        public DragDropListener() {
+
+        }
+
+
+        public void mousePressed(MouseEvent e) {
+            JComponent jc = (JComponent) e.getSource();
+            TransferHandler th = jc.getTransferHandler();
+            th.exportAsDrag(jc, e, TransferHandler.COPY);
+
+        }
+    }
+
+}
