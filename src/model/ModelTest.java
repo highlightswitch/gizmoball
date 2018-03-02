2ffbbda911aabfb51d40fa876d66771fc254b7dd
AHC <chadha.degachi.2015@uni.strath.ac.uk>
2018-03-02 23:49:43 +0000

    need to override clone for interfaces - still junits

--- src/model/ModelTest.java
+++ src/model/ModelTest.java
@@ -1,37 +1,64 @@
 package model;
 
+import model.gizmo.Ball;
+import model.gizmo.Flipper;
+import model.gizmo.Tickable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
+    Model m;
+    ArrayList<Collidable> collide;
+    ArrayList<Drawable> draw;
+    ArrayList<Tickable> tick;
+    Ball b;
+    Tile[][] t;
+    Flipper f;
+
     @BeforeEach
     void setUp() {
+        m =  new Model();
+        collide = m.getCollidable();
+        draw = m.getDrawables();
+        b = m.getBall();
+        t = m.getTiles();
     }
 
     @Test
-    void getDrawables() {
+    void getGizmoByName() throws GizmoNotFoundException {
+        //when retreving by name what do you if there is multiple gizmos with the same name
+        // is the gizmo equals method overridden
+       assertThrows(GizmoNotFoundException.class, () -> m.getGizmoByName("Blue"));
     }
 
     @Test
-    void getCollidable() {
-    }
+    void deleteGizmo() throws GizmoNotFoundException {
+        ArrayList<Collidable> c =  new ArrayList<>();
+        ArrayList<Drawable> d = new ArrayList<>();
+        ArrayList<Tickable> t = new ArrayList<>();
 
-    @Test
-    void getBall() {
-    }
+        for (Collidable col : collide){
+            c.add(col.clone());
+        }
 
-    @Test
-    void getTiles() {
-    }
+        for(Drawable dr : draw){
+            d.add(dr.clone());
+        }
 
-    @Test
-    void getGizmoByName() {
-    }
+        for (Tickable ti : tick){
+            t.add(ti.clone());
+        }
 
-    @Test
-    void deleteGizmo() {
+       m.deleteGizmo("Flipper");
+
+       assertEquals(c.size() - 1, collide.size());
+       assertEquals(d.size() - 1, draw.size());
+       assertEquals(t.size() - 1, tick.size());
+       assertThrows(GizmoNotFoundException.class, () -> m.getGizmoByName("Flipper"));
     }
 
     @Test
