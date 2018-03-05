eca46c261c3688320ef7320d1020d0720d040e59
Chadha <shatha.degachi@gmail.com>
2018-03-05 23:16:08 +0000

    started wall tests

--- src/model/WallsTest.java
+++ src/model/WallsTest.java
@@ -1,10 +1,19 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WallsTest {
+    Walls w;
+    Model m;
+
+    @BeforeEach
+    void setUp() {
+        m = new Model();
+        w  = new Walls(1,1,10,2);
+    }
 
     @Test
     void getGameObject() {
--- src/physics/GeometryTest.java
+++ src/physics/GeometryTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GeometryTest {
-
-    @Test
-    void setGeometry() {
-    }
+    Geometry g = new Geometry();
+    Vect x = new Vect(0,3);
+    Vect y = new Vect(4,4);
+    Geometry.VectPair v = new Geometry.VectPair(x,y);
 
     @Test
     void quadraticSolution() {
